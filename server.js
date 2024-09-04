9195da6350a1b3dfd6e8d791d52f80821273bff9
Isaac <isaacomar29@gmail.com>
2024-09-04 17:56:42 +0100

    initialisation du projet

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+
+const app = express();
+
+const port = process.env.PORT || 3000 ;
+
+
+
+
+app.listen(port, () => {
+    console.log(`Welcom to GABAO for port ${port}`)
+})
+  
